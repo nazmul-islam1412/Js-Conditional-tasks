a193149cbaba3f967e352895db69fb950008b19f
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-07-28 04:05:19 +0600

    added task3

---
+++ task3.js
@@ -0,0 +1,17 @@
+let num=85;
+
+if(num>=90 && num<=100){
+    console.log("A");
+}
+else if(num>=80 && num<=89){
+    console.log("B");
+}
+else if(num>=70 && num<=79){
+    console.log("C");
+}
+else if(num>=60 && num<=69){
+    console.log("D");
+}
+else if(num>=0 && num<=59){
+    console.log("F");
+}
